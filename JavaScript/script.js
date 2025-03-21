0e5ff6b00b5bc536e1b9a3ab878c243da0d0848e
joaopedrojv <pedrojunq2015@hotmail.com>
2025-03-20 22:18:49 -0300

    Att

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+
+       /* var primeiroTipo;
+        let segundoTipo;
+        const terceiroTipo = 10;
+
+        var nome = "joao";
+        var idade = 33;
+
+        console.log(nome);
+        console.log(idade); */
+
+    function escrever(){
+        document.getElementById('texto').innerText = "oi, vc me clicou"
+    }
+    
